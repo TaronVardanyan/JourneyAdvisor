16697a2de59b837c702e5a4312e2a1459a4ca65d
Taron Vardanyan <taron.vardanyan@betconstruct.com>
2022-07-11 18:42:54 +0400

    added matrial ui and rest API plugin

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,21 +4,31 @@ import Helmet from "react-helmet";
 import GlobalStyle from "../../styles/global";
 import Theme from "../../styles/theme";
 import { ThemeProvider } from "styled-components";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import {
+  createTheme,
+  ThemeProvider as MaterialThemeProvider,
+} from "@material-ui/core";
+
+const materialDarkTheme = createTheme();
 
 const Layout = ({ pageTitle, children }: LayoutProps) => {
   return (
-    <ThemeProvider theme={Theme}>
-      <Helmet
-        bodyAttributes={{
-          class: "body",
-        }}
-      >
-        <meta charSet="utf-8" />
-        <title>{pageTitle}</title>
-      </Helmet>
-      <GlobalStyle />
-      {children}
-    </ThemeProvider>
+    <MaterialThemeProvider theme={materialDarkTheme}>
+      <ThemeProvider theme={Theme}>
+        <Helmet
+          bodyAttributes={{
+            class: "body",
+          }}
+        >
+          <meta charSet="utf-8" />
+          <title>{pageTitle}</title>
+        </Helmet>
+        <CssBaseline />
+        <GlobalStyle />
+        {children}
+      </ThemeProvider>
+    </MaterialThemeProvider>
   );
 };
 
