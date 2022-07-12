d8a00e833db5bc8549e2f1d9a565ba85c3022b9e
Taron Vardanyan <taron.vardanyan@betconstruct.com>
2022-07-12 12:58:11 +0400

    add initial structure for project

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { HomePageWrapper } from "./styles";
-import { Button } from "@mui/material";
+import { HomePageWrapper, MaterialGrid } from "./styles";
+import Header from "./components/Header";
+import List from "./components/List";
+import Map from "./components/Map";
 
 const HomePage = () => {
   return (
     <HomePageWrapper>
-      HomePage <Button variant="contained">Text</Button>
+      <Header />
+      <MaterialGrid container spacing={3}>
+        <MaterialGrid item xs={12} md={4}>
+          <List />
+        </MaterialGrid>
+        <MaterialGrid item xs={12} md={8}>
+          <Map />
+        </MaterialGrid>
+      </MaterialGrid>
     </HomePageWrapper>
   );
 };
